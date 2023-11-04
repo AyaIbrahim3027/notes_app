a9d1cafa6b7ee9fbd6b093ff3d570649730d1712
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-11-04 15:38:52 +0200

    create custom note item

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'custom_app_bar.dart';
+import 'custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({Key? key}) : super(key: key);
@@ -11,12 +11,14 @@ class NotesViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: const [
-          SizedBox(height: 40,),
+          SizedBox(
+            height: 40,
+          ),
           CustomAppBar(),
+          NoteItem(),
         ],
       ),
     );
   }
 }
-
 
