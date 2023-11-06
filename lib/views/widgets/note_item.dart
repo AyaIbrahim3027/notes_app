e82fad33a42447f7f2b78db51691fab155a7793b
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-11-06 12:38:35 +0200

    refresh notes list

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 
 import '../edit_note_view.dart';
@@ -52,6 +54,8 @@ class NoteItem extends StatelessWidget {
                 ),
                 onPressed: () {
                   note.delete();
+
+                  BlocProvider.of<NotesCubit>(context).fetchAllNotes();
                 },
               ),
             ),
